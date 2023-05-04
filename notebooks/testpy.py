35f03fe50e571b1f1efdd75ab43ee36b657e1ccf
CyJERIDI <132550570+CyJERIDI@users.noreply.github.com>
2023-05-04 10:48:55 +0200

    Update testpy.py

--- notebooks/testpy.py
+++ notebooks/testpy.py
@@ -5,6 +5,22 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+df = pd.read_csv('https://github.com/CyJERIDI/Parking-ML/blob/codespace-cyjeridi-literate-computing-machine-gg66r7x9gvjhv5jj/data/verdun_MAJ.csv' )
+ 
+df['y'] = pd.array(df.y, dtype=pd.Int64Dtype())
+df['ds'] = pd.to_datetime(df.ds, format='%Y-%m-%d', errors='coerce')
+
+df1 = df.loc[df.ds <"2020-01-01"].copy()
+df2 =df.loc[df.ds> "2020-12-31"].copy()
+frames = [df1, df2]
+split_date = '2023-04-15'  
+df = pd.concat(frames)
+df_train = df.loc[df.ds <=split_date].copy()
+df_test =df.loc[df.ds> split_date].copy()
+
+
+
 st.title("A Simple Streamlit Web App")
 name = st.text_input("Enter your name", '')
 st.write(f"Hello {name}!")
