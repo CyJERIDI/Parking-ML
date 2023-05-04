bac53e2592e58a0f4b33143165c9f61f195f2cc5
CyJERIDI <132550570+CyJERIDI@users.noreply.github.com>
2023-05-04 10:50:58 +0200

    Update testpy.py

--- notebooks/testpy.py
+++ notebooks/testpy.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 #from sklearn.metrics import mean_squared_error, mean_absolute_error
 
-df = pd.read_csv('https://github.com/CyJERIDI/Parking-ML/blob/codespace-cyjeridi-literate-computing-machine-gg66r7x9gvjhv5jj/data/verdun_MAJ.csv' )
+df = pd.read_csv('data/verdun_MAJ.csv' )
  
 df['y'] = pd.array(df.y, dtype=pd.Int64Dtype())
 df['ds'] = pd.to_datetime(df.ds, format='%Y-%m-%d', errors='coerce')
