8736c636a0ce25b01615572cddfc0ce74bca0d2d
CyJERIDI <132550570+CyJERIDI@users.noreply.github.com>
2023-05-04 10:43:19 +0200

    Update testpy.py

--- notebooks/testpy.py
+++ notebooks/testpy.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
 import prophet
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, mean_absolute_error
 st.title("A Simple Streamlit Web App")
 name = st.text_input("Enter your name", '')
 st.write(f"Hello {name}!")
