04a1afaf2f93d57fab8054e880c50d0980eefe33
CyJERIDI <132550570+CyJERIDI@users.noreply.github.com>
2023-05-04 10:13:09 +0200

    Update streamlit_parking.py

--- notebooks/streamlit_parking.py
+++ notebooks/streamlit_parking.py
@@ -12,7 +12,7 @@ import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
-python -m pip install prophet
+pipenv install prophet
 import prophet
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
